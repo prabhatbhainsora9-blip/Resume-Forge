66af5105060037cf9318488d511997afbe7a5e36
Prabhat Singh Bhainsora <prabhatbhainsora9@gmail.com>
2026-08-13 10:21:33 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const dbConfig = {
     host: process.env.DB_HOST || 'localhost',
     port: Number(process.env.DB_PORT || 3306),
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
+    password: process.env.DB_PASSWORD || 'prabhu@3321',
     database: process.env.DB_NAME || 'resume_forge',
     waitForConnections: true,
     connectionLimit: 10,
